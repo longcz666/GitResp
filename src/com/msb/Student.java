61b11016728a09181ab0340cd281fb7b3f8f90de
longcz <2598635373@qq.com>
2022-07-03 17:34:24 +0800

    创建了一个学生对象

--- src/com/msb/Student.java
+++ src/com/msb/Student.java
@@ -3,4 +3,11 @@ package com.msb;
 public class Student {
     private Integer age;
     private String name;
+
+    public Student(Integer age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    Student s1 = new Student(16,"辉夜");
 }
